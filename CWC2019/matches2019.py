361ca0edb9376b7d8cfd3b4b01a27878638052c4
abhayvikramnayak98 <43579599+abhayvikramnayak98@users.noreply.github.com>
2023-10-20 11:39:50 +0530

    Ready to launch the 2019 stats.

---
+++ CWC2019/matches2019.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+
+# Send an HTTP GET request to the website
+url = "https://www.cricbuzz.com/cricket-series/2697/icc-cricket-world-cup-2019/matches"
+response = requests.get(url)
+
+# Parse the HTML content of the page using BeautifulSoup
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find the relevant data on the page
+match_data = []
+
+match_details = soup.find_all("div", class_="cb-col-60 cb-col cb-srs-mtchs-tm")
+for details in match_details:
+    match_info = details.find("span").text
+    if len(match_info.split(",")) == 3:
+        teams, match, group = match_info.split(",")
+    else:
+        teams, match = match_info.split(",")
+    team1, team2 = teams.split(" vs ")
+    team1, team2 = [team.title() for team in [team1, team2]]
+    result = details.find("a", class_="cb-text-complete").text.strip()
+    if 'tied' in result:
+        if 'Super Over' in result:
+            winner = result.split(" won ")[0].split('(')[-1]
+            margin = 'Super Over'
+        else:
+            winner = 'NR'
+            margin = 'Tied'
+    elif 'won' in result:
+        winner, margin = result.split(" won by ")
+    elif 'abandoned' in result:
+        winner = 'NR'
+        margin = 'Abandoned'
+    else:
+        winner = 'NR'
+        margin = 'No Result'
+    stadium = details.find("div", class_="text-gray").text.strip()
+    ground, venue = stadium.split(",")
+
+    match_data.append([team1, team2, winner, margin, ground, venue])
+
+# Create a pandas DataFrame
+df = pd.DataFrame(match_data, columns=["Team1", "Team2", "Winner", "Margin", "Ground", "Venue"])
+
+df.columns = [column.lower() for column in df.columns]
+
+# Export the data to a CSV file
+df.to_csv(os.path.join("CWC2019", "icc_world_cup_2019.csv"), index=False)
+
+print("Data scrapped successfully.")
